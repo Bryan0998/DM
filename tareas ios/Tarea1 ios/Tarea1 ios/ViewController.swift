ace772b48fb5229b2f114ffb897492e5edfd9af2
BRYAN MISAEL RODRIGUEZ PORTILLO <16170717@itculiacan.edu.mx>
2019-09-27 13:51:39 -0600

    ios

---
+++ tareas ios/Tarea1 ios/Tarea1 ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarea1 ios
+//
+//  Created by CCDM19 on 9/27/19.
+//  Copyright © 2019 CCDM19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
